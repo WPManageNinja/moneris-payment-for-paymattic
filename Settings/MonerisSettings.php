80a242ede7e5b004d1e2b1610606eaedaef2bd3a
AKM Elias <akmelias11@gmail.com>
2024-03-28 15:41:30 +0600

    Revert "refactor: checkupdate without authorization"
    This reverts commit beec66370268680eaa2df645ff5869da72a85a43.

--- Settings/MonerisSettings.php
+++ Settings/MonerisSettings.php
@@ -158,18 +158,31 @@ class MonerisSettings extends BasePaymentMethod
         $classicToken = 'ksosCDd6BqJswQNb8e0qnrXcAGEdyw0OOKul';
         $fineGrainedToken = 'pat_11AGXI3WI04eyOUnGE7HY8_1rf0oWy8PkDKZRaCUgPQjE3dohmO0fCjNmeNpUNqufSVSGMXD3AoAdyAtzJ';
 
-        // $response = wp_remote_get($githubApi, 
-        // [
-        //     'headers' => array(
-        //         'Accept' => 'application/json',
-        //         'authorization' => 'Bearer ' . 'ghp_' . $classicToken
-        //     )
-        // ]);
+        $response = wp_remote_get($githubApi, 
+        [
+            'headers' => array(
+                'Accept' => 'application/json',
+                'authorization' => 'Bearer ' . 'ghp_' . $classicToken
+            )
+        ]);
 
         $response = wp_remote_get($githubApi);
         $releases = json_decode($response['body']);
+
         if (isset($releases->documentation_url)) {
-            return $result;
+            // lest try with other token
+            $response = wp_remote_get($githubApi, 
+            [
+                'headers' => array(
+                    'Accept' => 'application/json',
+                    'authorization' => 'Bearer ' . 'github_'. $fineGrainedToken
+                )
+            ]);
+            $response = wp_remote_get($githubApi);
+            $releases = json_decode($response['body']);
+            if (isset($releases->documentation_url)) {
+                return $result;
+            }
         }
 
         $latestRelease = $releases[0];
